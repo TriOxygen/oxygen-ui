c0550b29eca00b4e623895bb3db087f1213f817d
Öz <ozgur.gene@gmail.com>
2016-04-25 10:59:01 +0200

    Simplify layout Component

--- src/Layout.js
+++ src/Layout.js
@@ -4,27 +4,13 @@ import View from './View';
 class Layout extends Component {
   static propTypes = {
     children: PropTypes.node,
-    header: PropTypes.node,
-    fixedHeader: PropTypes.bool,
-    onContentScroll: PropTypes.func,
-    theme: PropTypes.object
-  };
-
-  static contextTypes = {
-    theme: PropTypes.object,
-  };
-
-  static defaultProps = {
-    fixedHeader: true
   };
 
   render() {
     const { children, ...rest } = this.props;
-    const [header, ...otherChildren] = children;
     return (
       <View column className={layoutStyles.root} {...rest}>
-        {header}
-        {otherChildren}
+        {children}
       </View>
     );
   }
