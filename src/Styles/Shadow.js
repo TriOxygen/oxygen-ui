16724c32c225931727cf418f8c8a91912d5ab639
Öz <ozgur.gene@gmail.com>
2016-04-26 14:54:53 +0200

    Prettier shadows

--- src/Styles/Shadow.js
+++ src/Styles/Shadow.js
@@ -1,9 +1,8 @@
 export default [
   null,
-  '0 2px 4px rgba(0,0,0,0.12), 0 2px 4px rgba(0,0,0,0.24)',
-  '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-  '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-  '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
-  '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'
+  '0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12)',
+  '0 3px 10px rgba(0,0,0,0.16), 0 3px 10px rgba(0,0,0,0.23)',
+  '0 10px 30px rgba(0,0,0,0.19), 0 6px 10px rgba(0,0,0,0.23)',
+  '0 14px 45px rgba(0,0,0,0.25), 0 10px 18px rgba(0,0,0,0.22)',
+  '0 19px 60px rgba(0,0,0,0.30), 0 15px 20px rgba(0,0,0,0.22)'
 ];
-
